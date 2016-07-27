afbbf73f80c1321f06b525d11fd6a8dfba146ffd
lsvih <408772145@qq.com>
2016-07-27 12:02:44 +0800

    Index Style

--- js/data.js
+++ js/data.js
@@ -3,31 +3,31 @@ gFlow = {
 	"flow": [{
 		"id": 1,
 		"name": "待上门",
-		"icon": "eye",
+		"icon": "1",
 		"num": 0,
 		"contain-event": [0, 1]
 	}, {
 		"id": 2,
 		"name": "待支付",
-		"icon": "compose",
+		"icon": "2",
 		"num": 0,
 		"contain-event": [2, 3, 4]
 	}, {
 		"id": 3,
 		"name": "待开工",
-		"icon": "redo",
+		"icon": "3",
 		"num": 0,
 		"contain-event": [5, 6, 7]
 	}, {
 		"id": 4,
 		"name": "施工中",
-		"icon": "star",
+		"icon": "4",
 		"num": 0,
 		"contain-event": [8]
 	}, {
 		"id": 5,
 		"name": "待评价",
-		"icon": "chatbubble",
+		"icon": "5",
 		"num": 0,
 		"contain-event": [9]
 	}]
